90f47f80fe243d4003ac88fc4298e0a7edcc214c
Sole1999 <1ccontapatsyrios@gmail.com>
2025-06-14 00:53:46 -0500

    Reto 3 registrado los inserts en el script.

---
+++ inserciones/scripts.sql
@@ -0,0 +1,174 @@
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (1,'escoba','palo de madera',3.50,52)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (2,'jarra','plastico de 1 litro',1.80,68)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (3,'mouse','color negro',8.98,45)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (4,'botella de agua','250 ml',0.70,92)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (5,'foco','luz led blanca',2.55,85)
+-----------------------------------------------------------------------------
+
+insert into productos (codigo,nombre,precio,stock)
+values (6,'linterna',12.75,89)
+
+insert into productos (codigo,nombre,precio,stock)
+values (7,'camisa',4.75,71)
+
+insert into productos (codigo,nombre,precio,stock)
+values (8,'zapatos deportivos',38.00,120)
+
+----------------------------------------------------------------------------
+
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (45210,12458,'25/10/2014',120.65)
+
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (78910,84598,'26/01/2010',9650.05)
+
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (96325,24536,'03/08/2006',469.00)
+
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (36527,54321,'17/11/2017',2150.95)
+
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (96854,82145,'05/09/2024',1290.75)
+
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (65842,54786,'19/03/2018',104.87)
+
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (04589,27488,'12/06/2009',254.15)
+
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (45710,82458,'20/12/2015',1200.65)
+
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (10102,12129,'05/02/2023',750.12)
+
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (21020,45851,'23/07/2013',3540.01)
+
+---------------------------------------------------------------------------
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (5241963210,'Sebastian','Almeida','sbastal@email.com','02/02/2008')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (0952634110,'Angelica','Perez','angelpe@email.com','24/09/2009')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (1036527557,'Johan','Ramos','johanr@email.com','16/12/2008')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (0963524178,'Sara','Quiroz','saraqz@email.com','14/07/2008')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (4521632041,'Javier','Zapata','javizp@email.com','19/11/2009')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (0952463210,'Carlos','Caicedo','carloscd@email.com','22/04/2008')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (9641963210,'Marcos','Sepeda','sepedamarc@email.com','23/03/2009')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (3210635241,'Jacqueline','Moran','jacqmr@email.com','03/06/2009')
+
+---------------------------------------------------------------------------
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (1,5241759632,'24/04/2025','10:04')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (2,9685324175,'07/09/2020','12:45')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (3,5241659632,'17/02/2022','19:07')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (4,5321759632,'01/09/2024','08:12')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (5,5963252741,'16/12/2018','14:27')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (6,0985763201,'29/07/2023','17:45')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (7,0725417586,'21/10/2020','12:07')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (8,0956847520,'07/01/2015','11:54')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (9,5485963210,'17/04/2011','09:21')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (10,0952632041,'29/05/2021','18:06')
+
+---------------------------------------------------------------------------
+
+insert into videojuegos (codigo,nombre,descripcion,valoracion)
+values (1,'Resident Evil ','Juego de muertos vivientes',8)
+
+insert into videojuegos (codigo,nombre,descripcion,valoracion)
+values (2,'Minecraft ','Juego de mundo en cuadros',7)
+
+insert into videojuegos (codigo,nombre,descripcion,valoracion)
+values (3,'Resident Evil 2','Juego de muertos vivientes',9)
+
+insert into videojuegos (codigo,nombre,descripcion,valoracion)
+values (4,'Mario Bros','Juego de supervivencia',10)
+
+insert into videojuegos (codigo,nombre,descripcion,valoracion)
+values (5,'Pac-Man','Juego de comer todo sin ser comido',10)
+
+---------------------------------------------------------------------------
+
+insert into videojuegos (codigo,nombre,valoracion)
+values (6,'Sonic',8)
+
+insert into videojuegos (codigo,nombre,valoracion)
+values (7,'Solo Leveling',9)
+
+insert into videojuegos (codigo,nombre,valoracion)
+values (8,'Call of duty',10)
+
+---------------------------------------------------------------------------
+
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (1,62514,142.30,'D','10/05/2021','10:41')
+
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (2,32415,251.04,'C','14/09/2022','17:12')
+
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (3,41526,174.45,'C','11/07/2024','14:24')
+
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (4,85014,651.71,'D','07/12/2020','08:24')
+
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (5,20415,24.00,'D','03/03/2023','14:22')
+
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (6,95847,580.09,'C','18/10/2024','18:57')
+
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (7,96352,954.47,'C','02/01/2025','22:04')
+
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (8,95624,20.84,'D','01/01/2025','09:20')
+
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (9,84752,100.00,'C','19/08/2021','06:42')
+
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (10,35214,45.50,'D','04/11/2020','12:12')
